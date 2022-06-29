9cd65a61c0aeea74d7db09be91369886f372a34e
JoeDDev <josdarotblau@gmail.com>
2022-06-29 13:02:42 -0600

    pyrebase

--- api/views.py
+++ api/views.py
@@ -1,14 +1,49 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from urllib.parse import quote
+
+import pyrebase
+import json
+import random
+import string
+
+firebaseConfig ={"apiKey": "AIzaSyCRvKDk0ixXieZataevh6h__eWN0xhEwII",
+  "authDomain": "bamdata-4bf86.firebaseapp.com",
+  "databaseURL":"https://bamdata-4bf86-default-rtdb.firebaseio.com/",
+  "projectId": "bamdata-4bf86",
+  "storageBucket": "bamdata-4bf86.appspot.com",
+  "messagingSenderId": "178450080103",
+  "appId": "1:178450080103:web:9e0eea97d0a37d91b10fc2"}
 
 @api_view(['GET'])
 def getData(request):
-    person = {'name': 'Angie', 'age': 25}
-    #print(request.data['prueba'])
+
+    pyrebase.pyrebase.quote = lambda s, safe=None: s
+    def get_url(self, token=None):
+        path = self.path
+        self.path = None
+        if path.startswith('/'):
+            path = path[1:]
+        if token:
+            return "{0}/o/{1}?alt=media&token={2}".format(self.storage_bucket, quote(path, safe=''), token)
+        return "{0}/o/{1}?alt=media".format(self.storage_bucket, quote(path, safe=''))
+
+    pyrebase.pyrebase.Storage.get_url = lambda self, token=None: \
+    get_url(self, token)
+
+    cliente = request.data['cliente']
+    valor = {"documento":'documento'}
+    documento = valor["documento"]
+    val = "0"
+    print(cliente)
+
+    firebase = pyrebase.initialize_app(firebaseConfig)
+    db=firebase.database()
+    print("va")
+
+    result = db.child("Clientes").order_by_child(documento).equal_to(cliente).get()
+    print("--->", result.val())
+
     error = 'Error log'
-    ''' 
-    if request.data['prueba'] == 1290:
-        return Response(person)
-    return Response(error)
-    '''
-    return Response(person)
+   
+    return Response(result.val())
--- bam_api/settings.py
+++ bam_api/settings.py
@@ -24,11 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-70(i1@(3krmjwoi398mzad@1e^@ec05m6guglbzg@1@**s48tw'
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-70(i1@(3krmjwoi398mzad@1e^@ec05m6guglbzg@1@**s48tw'
+#SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["https://bam-data.herokuapp.com/"]
 
